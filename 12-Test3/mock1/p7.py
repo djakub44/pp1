54f55fff61bf9a777b351c508ac9979133ba23d8
djakub44 <djakub44@gmail.com>
2024-01-10 23:28:42 +0100

    nauka

--- 12-Test3/mock1/p7.py
+++ 12-Test3/mock1/p7.py
@@ -1,7 +1,17 @@
 def f(arr2D):
-    return False
+    arrSum = [0]*len(arr2D[0])
+    for i in arr2D:
+        for j in range(len(i)):
+            arrSum[j]+=i[j]
 
-
+    for i in range(len(arrSum)):
+        for j in range(i+1, len(arrSum)):
+            if arrSum[i] == arrSum[j]:
+                return True
+    else:
+        return False
 
 if __name__ == "__main__":
+    #print(f([[3,4,2],[5,1,6]]))
     print(f([[3,4,2],[5,1,6]]))
+    print(f([[3,4,2],[5,1,7]]))
